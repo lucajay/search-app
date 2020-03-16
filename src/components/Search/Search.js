b6778e10e7301def147ac1adae12df8d611db0cb
Ajay Verma <ajayvinay2012@gmail.com>
2020-03-16 18:01:10 +0530

    Update Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,24 +2,26 @@ import React, { useState } from 'react';
 import './Search.css';
 
 function Search(props) {
-  const [ title, setTitle ] = useState('');
+  const [ text, setText ] = useState('');
   const [ type, setType ] = useState('all');
 
   let handleSearch = function (e) {
-    props.onSearchMovie({title: title, type: type});
+    if(text) {
+      props.onSearchMovie({text: text, type: type});
+    }
     e.preventDefault();
   }
   return (
     <div className="search-panel">
       <form onSubmit={handleSearch}>
-        <input type="title" name="title" onChange={(e) => setTitle(e.target.value)}/>
+        <input type="text" name="text" onChange={(e) => setText(e.target.value)}/>
         <select id="type" name="type" onChange={(e) => setType(e.target.value)}>
           <option value="all" defaultChecked>All</option>
           <option value="movie">Movies</option>
           <option value="series">Series</option>
           <option value="episode">Episodes</option>
         </select>
-        <input type="submit" value="Search"/>
+        <input type="submit" className="search-button" value="Search"/>
       </form>
     </div>
   );
