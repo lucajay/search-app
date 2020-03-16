122d7effdccac2a24aff91d2522ca98c81a88a6d
Ajay Verma <ajayvinay2012@gmail.com>
2020-03-16 17:46:03 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './Header.css';
 
 function Header(props) {
-  const showFavHandler = (e, value) => {
-    e.preventDefault();
-    const showFav = 1 - value;
-    props.showFavCallback(showFav);
-  }
+
   return (
     <header className="App-header">
       <div className="nav-btns">
@@ -14,7 +10,7 @@ function Header(props) {
         <a className="nav-btn" href="/">Home</a>
         </div>
         <div className="nav-right">
-          <a className="nav-btn" onClick={(e) => showFavHandler(e, props.showFav)}>Favourite</a>
+          <a className="nav-btn" href='/fav'>Favourite</a>
         </div>
       </div>
   </header>
